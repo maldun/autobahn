54392f7297a713fabd60bfcae085ebead12b21e7
Mal'dun Finsterschreck <Stefan Reiterer>
2022-07-09 19:45:53 +0200

    added autogenerated ideas

--- .scripts/general_replacements.py
+++ .scripts/general_replacements.py
@@ -9,7 +9,7 @@ from Hoi4Converter.converter import *
 
 HOI4_FOLDER = HOME + ".local/share/Steam/steamapps/common/Hearts of Iron IV/"
 KR_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/1521695605/"
-KX_FOLDER = ".local/share/Steam/steamapps/workshop/content/394360/2076426030/"
+KX_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/2076426030/"
 RT56_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/820260968/"
 
 KX = True
@@ -17,6 +17,7 @@ OUT_FOLDER = HOME + ".local/share/Paradox Interactive/Hearts of Iron IV/mod/auto
 MAIN_MOD = KX_FOLDER if KX is True else KR_FOLDER
 
 IDEA_PATH = "common/ideas"
+HISTORY_COUNTRY_PATH = "history/countries"
 
 ideologies = {"fascism": ("national_populist", "paternal_autocrat"),
                   "democratic": ("social_democrat", "social_liberal",
@@ -26,9 +27,6 @@ ideologies = {"fascism": ("national_populist", "paternal_autocrat"),
                   }
 
 def ideology_map():
-    file_list = os.listdir(RT56_FOLDER + IDEA_PATH)
-    file_list = [fname for fname in file_list if fname.startswith("r56i_laws_")]
-
     maps = []
     # update fascist
     key = "has_government"
@@ -44,7 +42,7 @@ def ideology_map():
         maps.append([[has_key_and_val, ikey], [remove, ikey]])
 
     # remove traits which are obsolete
-    trait_list = ["communist_revolutionary", "democratic_reformer", "fascist_demagogue" ]
+    trait_list = ["communist_revolutionary", "democratic_reformer", "fascist_demagogue", "womens_figurehead"]
     traits = [["has_idea_with_trait" , [trait]] for trait in trait_list]
     for ikey in traits:
         maps.append([[has_key_and_val, ikey], [remove, ikey]])
@@ -62,6 +60,18 @@ def ideology_map():
     val = "TUR"
     val2 = "OTT"
     maps.append([[has_key_and_val, [key, [val]]], [add_multiple, [[key, [val2]]]]])
+
+    # Add RadSoc when anarchist Commune is there
+    val = ['has_country_leader', [['name', ['"Anarchist Commune"']], ['ruling_only', [True]]]]
+    mapping = [[has_value, val],
+               [add_multiple, [["has_government",["radical_socialist"]]]]
+               ]
+    maps.append(mapping)
+    return maps
+
+def apply_ideology_map(maps):
+    file_list = os.listdir(RT56_FOLDER + IDEA_PATH)
+    file_list = [fname for fname in file_list if fname.startswith("r56i_laws_")]
     
     for file in file_list:
         os.makedirs(os.path.join(OUT_FOLDER, IDEA_PATH), exist_ok=True)
@@ -70,8 +80,35 @@ def ideology_map():
                            os.path.join(OUT_FOLDER, IDEA_PATH, file),
                            maps)
 
+def remove_obsolete_equipment_maps():
+    maps = []
+    key_and_val = ["cv_naval_bomber1", [1]]
+    maps.append([[has_key_and_val, key_and_val],
+                 [remove, key_and_val]])
+
+    return maps
     
+def apply_equipment_maps(maps):
+    in_folder = os.path.join(MAIN_MOD, HISTORY_COUNTRY_PATH)
+    out_folder = os.path.join(OUT_FOLDER, HISTORY_COUNTRY_PATH) 
+    file_list = os.listdir(in_folder)
+    #file_list = ["FRA - France.txt"]
+    os.makedirs(out_folder,exist_ok=True)
+
+    for file in file_list:
+        try:
+            #import pdb; pdb.set_trace()
+
+            apply_maps_on_file(os.path.join(in_folder, file),
+                           os.path.join(out_folder, file),
+                           maps)
+        except:
+            import pdb; pdb.set_trace()
+
     
 if __name__ == "__main__":
     os.makedirs(OUT_FOLDER, exist_ok=True)
-    ideology_map()
+    maps = ideology_map()
+    apply_ideology_map(maps)
+    maps = remove_obsolete_equipment_maps()
+    apply_equipment_maps(maps)

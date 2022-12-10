c72d048f11466704815a5676e3e0742a23c2faa4
Mal'dun Finsterschreck <Stefan Reiterer>
2022-12-10 02:38:29 +0100

    updated infantry stuff

--- .scripts/rt56_patches.py
+++ .scripts/rt56_patches.py
@@ -166,6 +166,199 @@ tank_patches += ["""
 
 
 #######################################################################
+# Patches for Infantry tech                                           #
+#######################################################################
+infantry_snippets =[]
+infantry_patches =[]
+
+infantry_snippets += ["""OR = {
+				    tag = CHI
+				    tag = PRC
+				    tag = XSM
+				    tag = SIK
+				    tag = GXC
+				    tag = SHX
+				    tag = YUN
+				}
+                       """]
+infantry_patches += ['']
+infantry_snippets += ["tag = USA"]
+infantry_patches += ['original_tag = USA']
+infantry_snippets += ["""
+                      OR = {
+				tag = BRA
+				tag = VEN
+				tag = COG
+				tag = INS
+				tag = MAL
+				tag = SIA
+				tag = RAJ
+				tag = AST
+				tag = SAF
+				}"""]
+infantry_patches += ["""
+                      OR = {
+				tag = BRA
+				tag = VEN
+				tag = COG
+				tag = INS
+				tag = MAL
+				tag = SIA
+				tag = RAJ
+				tag = AST
+				tag = SAF
+                                tag = GEA
+                                tag = PRF
+                                tag = HND
+                                tag = MAF
+				}"""]
+infantry_snippets += ["""
+                     OR = {
+                    original_tag = CAN
+                    original_tag = SWE
+                    original_tag = FIN
+                    original_tag = SAM
+                    original_tag = GRL
+                    original_tag = ICE
+                    original_tag = QBC
+                    original_tag = RUS }
+                    """]
+infantry_patches += ["""
+                     OR = {
+                    original_tag = CAN
+                    original_tag = SWE
+                    original_tag = FIN
+                    original_tag = ICE
+                    original_tag = RUS
+                    original_tag = BAT
+                    original_tag = TRM
+                    } """]
+infantry_snippets += ["""
+                      OR = { 
+                        original_tag = SWE
+                        original_tag = FIN
+                        original_tag = SAM
+                        original_tag = GRL
+                        original_tag = ICE
+                        original_tag = QBC
+                }
+                      """]
+infantry_patches += ["""
+                      OR = { 
+                        original_tag = SWE
+                        original_tag = FIN
+                        original_tag = ICE
+                      }
+                      """]
+infantry_snippets += ["""
+                     OR = {
+                    tag = SAU
+                    tag = IRQ
+                    tag = EGY
+                    tag = SAF
+                    tag = SYR
+                    tag = OMA
+                    tag = LBA
+                    tag = YEM
+                     }
+                     """]
+infantry_patches += ["""
+                     OR = {
+                       tag = SAU
+                       tag = IRQ
+                       tag = JBS
+                       tag = EGY
+                       tag = SAF
+                       tag = SYR
+                       tag = OMA
+                       tag = LIB
+                       tag = YEM
+                       tag = TUR
+                       tag = NFA
+                     }
+                     """]
+infantry_snippets += ["""
+                      modifier = {
+                factor = 3
+                OR = {
+                    original_tag = IRQ
+                    original_tag = KUR
+                    original_tag = PER
+                    original_tag = SAU
+                    original_tag = OMA
+                    original_tag = UAE
+                    original_tag = QAT
+                    original_tag = KUW
+                    original_tag = JOR
+                    original_tag = PAL
+                    original_tag = ISR
+                    original_tag = SYR
+                    original_tag = LEB
+                    original_tag = YEM
+                    original_tag = RAJ
+                    original_tag = SRL
+                    original_tag = PAK
+                    original_tag = BAN
+                    original_tag = HYD
+                    original_tag = BYA
+                    original_tag = SIA
+                    original_tag = LAO
+                    original_tag = CAM
+                    original_tag = VIN
+                    original_tag = INS
+                    original_tag = PHI
+                    original_tag = PNG
+                    original_tag = MAL
+                    original_tag = FRI
+                    original_tag = AST
+                    original_tag = MOR
+                    original_tag = ALG
+                    original_tag = TUN
+                    original_tag = LBA
+                    original_tag = EGY
+                    original_tag = SUD
+                    original_tag = ETH
+                    original_tag = DJI
+                    original_tag = ERI
+                    original_tag = SOM
+                    original_tag = SEN
+                    original_tag = MLI
+                    original_tag = NGA
+                    original_tag = TZN
+                    original_tag = MLW
+                    original_tag = RWA
+                    original_tag = BRD
+                    original_tag = CAR
+                    original_tag = CHA
+                    original_tag = CMR
+                    original_tag = GAB
+                    original_tag = RCG
+                    original_tag = ANG
+                    original_tag = ZAM
+                    original_tag = ZIM
+                    original_tag = BOT
+                    original_tag = NMB
+                    original_tag = SAF
+                    original_tag = MZB
+                    original_tag = MAD
+                    original_tag = KEN
+                    original_tag = UGA
+                    original_tag = COG
+                    original_tag = BRA
+                    original_tag = VEN
+                    original_tag = CAY
+                    original_tag = SUR
+                    original_tag = GYA
+                    original_tag = PAR
+                    original_tag = BOL
+                    
+                }
+                
+            }"""]
+infantry_patches += ['']
+
+
+#######################################################################
 # Patches for MTG Naval tech                                          #
 #######################################################################
 

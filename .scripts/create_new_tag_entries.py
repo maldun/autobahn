23de9a0a21cad33c49ce6e1549be3a1eb7b40d78
Mal'dun Finsterschreck <Stefan Reiterer>
2022-12-19 02:45:49 +0100

    updated localisation/gfx to IMP tag

--- .scripts/create_new_tag_entries.py
+++ .scripts/create_new_tag_entries.py
@@ -18,11 +18,17 @@ OUT_FOLDER = HOME + ".local/share/Paradox Interactive/Hearts of Iron IV/mod/auto
 
 
 INTERFACE_PATH = "interface"
+LOCALISATION_PATH = "localisation/english"
 GFX_SUFF = '.gfx'
+YML_SUFF = '.yml'
 NAME_KEY = "name"
 GFX_PREFIX = "GFX_"
+EQUIP_KEY = "equip"
 full_interface_path = os.path.join(ORG_FOLDER, INTERFACE_PATH)
 out_interface_path = os.path.join(OUT_FOLDER, INTERFACE_PATH)
+
+full_localisation_path = os.path.join(ORG_FOLDER, LOCALISATION_PATH)
+out_localisation_path = os.path.join(OUT_FOLDER, LOCALISATION_PATH)
 
 
 def duplicate_tags(fname, tag1, tag2):
@@ -50,7 +56,7 @@ def duplicate_tags(fname, tag1, tag2):
     return content
 
 
-def duplicate_gfx_files(tag1, tag2, suffix, in_path, out_path):
+def duplicate_gfx_files(tag1, tag2, in_path, out_path, suffix = GFX_SUFF):
     for root, _, files in os.walk(in_path):
         for file in files:
             if file.endswith(suffix):
@@ -62,6 +68,28 @@ def duplicate_gfx_files(tag1, tag2, suffix, in_path, out_path):
                     with open(out_fname, 'w') as fp:
                         fp.write(content)
 
+def duplicate_localisation_entries(in_file, tag1, tag2):
+    with open(in_file, 'r') as fp:
+        lines = fp.readlines()
+        new_lines = [line.replace(tag1,tag2) for line in lines if line.lstrip().startswith(f'{tag1}')]
+    lines += [f"\n# Added new country {tag2}\n"]
+    lines += new_lines
+
+    return lines
+    
+    
+                        
+def duplicate_localisation(tag1, tag2, in_path, out_path, suff=YML_SUFF):
+    for root, _, files in os.walk(in_path):
+        for file in files:
+            in_file = os.path.join(root, file)
+            out_file = os.path.join(root.replace(in_path, out_path), file)
+            if EQUIP_KEY in file.lower() and file.endswith(suff):
+                lines = duplicate_localisation_entries(in_file, tag1, tag2)
+                with open(out_file, 'w') as fp:
+                    fp.writelines(lines)
+
+                
 if __name__ == "__main__":
-    duplicate_gfx_files("CAN", "IMP", GFX_SUFF,
-                        full_interface_path, out_interface_path)
+    duplicate_localisation("CAN", "IMP", full_localisation_path, out_localisation_path)
+    duplicate_gfx_files("CAN", "IMP", full_interface_path, out_interface_path)
